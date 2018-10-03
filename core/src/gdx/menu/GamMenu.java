2ddd23f25222be4075eda6d0dcb9b0d74a80aea9
ndimax4 <com4uter$ci>
2018-10-03 12:04:23 -0400

    New with Ignore

--- core/src/gdx/menu/GamMenu.java
+++ core/src/gdx/menu/GamMenu.java
@@ -1,23 +1,13 @@
 package gdx.menu;
-import java.util.Iterator;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.Array;
+
 import gdx.menu.Screens.ScrCalculator;
 import gdx.menu.Screens.ScrMenu;
 import gdx.menu.Screens.ScrMessages;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
+
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.TimeUtils;
+
 import gdx.menu.Screens.ScrSimple;
 
 
@@ -27,15 +17,7 @@ public class GamMenu extends Game {
     ScrCalculator scrCalculator;
     ScrSimple scrSimple;
     int nScreen; // 0 for menu, 1 for play, and 2 for game over
-    private Texture Token;
-    private Texture Bag;
-    private Sound dropSound;
-    private Music BackMusic;
-    private SpriteBatch batch;
-    private OrthographicCamera camera;
-    private Rectangle bag;
-    private Array<Rectangle> tokens;
-    private long lastDropTime;
+
     
     public void updateState(int _nScreen) {
         nScreen = _nScreen;
@@ -58,87 +40,17 @@ public class GamMenu extends Game {
         scrCalculator = new ScrCalculator(this);
         scrSimple = new ScrSimple(this);
         updateState(0);
-        Token = new Texture(Gdx.files.internal("token.png"));
-        Bag = new Texture(Gdx.files.internal("money-bag.png"));
-
-        dropSound = Gdx.audio.newSound(Gdx.files.internal("coin.wav"));
-        BackMusic = Gdx.audio.newMusic(Gdx.files.internal("backmusic.mp3"));
-
-        BackMusic.setLooping(true);
-        BackMusic.play();
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
-        batch = new SpriteBatch();
-
-        bag = new Rectangle();
-        bag.x = 800 / 2 - 64 / 2;
-        bag.y = 20;
-        bag.width = 80;
-        bag.height = 80;
-
-        tokens = new Array<Rectangle>();
-        spawnToken();
-    }
-
-    private void spawnToken() {
-        Rectangle token = new Rectangle();
-        token.x = MathUtils.random(0, 800-64);
-        token.y = 480;
-        token.width = 50;
-        token.height = 50;
-        tokens.add(token);
-        lastDropTime = TimeUtils.nanoTime();
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClearColor(1, 0, 1, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        camera.update();
-
-        batch.setProjectionMatrix(camera.combined);
-
-        batch.begin();
-        batch.draw(Bag, bag.x, bag.y);
-        for (Rectangle token : tokens) {
-            batch.draw(Token, token.x, token.y);
-        }
-        batch.end();
-
-        if (Gdx.input.isTouched()) {
-            Vector3 touchPos = new Vector3();
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(touchPos);
-            bag.x = touchPos.x - 64 / 2;
-        }
-        if (Gdx.input.isKeyPressed(Keys.LEFT)) bag.x -= 200 * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Keys.RIGHT)) bag.x += 200 * Gdx.graphics.getDeltaTime();
-
-        if (bag.x < 0) bag.x = 0;
-        if (bag.x > 800 - 64) bag.x = 800 - 64;
-        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnToken();
-
-        for (Iterator<Rectangle> iter = tokens.iterator(); iter.hasNext(); ) {
-            Rectangle token = iter.next();
-            token.y -= 200 * Gdx.graphics.getDeltaTime();
-            if (token.y + 64 < 0) iter.remove();
-            if (token.overlaps(bag)) {
-                dropSound.play();
-                iter.remove();
             }
-        }
-        super.render();
-    }
+
 
     @Override
     public void dispose() {
-        Token.dispose();
-        Bag.dispose();
-        dropSound.dispose();
-        BackMusic.dispose();
-        batch.dispose();
         super.dispose();
     }
 }
--- core/src/gdx/menu/Screens/ScrMessages.java
+++ core/src/gdx/menu/Screens/ScrMessages.java
@@ -1,16 +1,28 @@
 package gdx.menu.Screens;
  import com.badlogic.gdx.Gdx;
+ import com.badlogic.gdx.Input;
  import com.badlogic.gdx.InputProcessor;
  import com.badlogic.gdx.Screen;
+ import com.badlogic.gdx.audio.Music;
+ import com.badlogic.gdx.audio.Sound;
  import com.badlogic.gdx.graphics.GL20;
+ import com.badlogic.gdx.graphics.OrthographicCamera;
+ import com.badlogic.gdx.graphics.Texture;
  import com.badlogic.gdx.graphics.g2d.BitmapFont;
  import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+ import com.badlogic.gdx.math.MathUtils;
+ import com.badlogic.gdx.math.Rectangle;
+ import com.badlogic.gdx.math.Vector3;
  import com.badlogic.gdx.scenes.scene2d.Actor;
  import com.badlogic.gdx.scenes.scene2d.Stage;
  import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+ import com.badlogic.gdx.utils.Array;
+ import com.badlogic.gdx.utils.TimeUtils;
  import gdx.menu.GamMenu;
  import gdx.menu.TbMenu;
  import gdx.menu.TbsMenu;
+
+ import java.util.Iterator;
 
 public class ScrMessages implements Screen, InputProcessor {
     GamMenu gamMenu;
@@ -18,7 +30,18 @@ public class ScrMessages implements Screen, InputProcessor {
     TbMenu tbMenu, tbCalculator, tbSimple;
     Stage stage;
     BitmapFont screenName;
-    SpriteBatch batch;
+    private Texture Token;
+    private Texture Bag;
+    private Sound dropSound;
+    private Music BackMusic;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Rectangle bag;
+    private Array<Rectangle> tokens;
+    private long lastDropTime;
+    private int score;
+    private String yourScoreName;
+    BitmapFont yourBitmapFontName;
 
     public ScrMessages(GamMenu _gamMenu) {  //Referencing the main class.
         gamMenu = _gamMenu;
@@ -43,13 +66,85 @@ public class ScrMessages implements Screen, InputProcessor {
         Gdx.input.setInputProcessor(stage);
         btnMenuListener();
         btnGameoverListener();
+        score = 0;
+        yourScoreName = "score: 0";
+        yourBitmapFontName = new BitmapFont();
+        Token = new Texture(Gdx.files.internal("token.png"));
+        Bag = new Texture(Gdx.files.internal("money-bag.png"));
+
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("coin.wav"));
+        BackMusic = Gdx.audio.newMusic(Gdx.files.internal("backmusic.mp3"));
+
+        BackMusic.setLooping(true);
+        BackMusic.play();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1280, 720);
+        batch = new SpriteBatch();
+
+        bag = new Rectangle();
+        bag.x = 800 / 2 - 64 / 2;
+        bag.y = 20;
+        bag.width = 80;
+        bag.height = 80;
+
+        tokens = new Array<Rectangle>();
+        spawnToken();
     }
+
+    private void spawnToken() {
+        Rectangle token = new Rectangle();
+        token.x = MathUtils.random(0, 800-64);
+        token.y = 480;
+        token.width = 50;
+        token.height = 50;
+        tokens.add(token);
+        lastDropTime = TimeUtils.nanoTime();
+    }
+
 
     public void render(float delta) {
                 stage.act();
                 stage.draw();
-            }
 
+        camera.update();
+
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        yourBitmapFontName.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+        yourBitmapFontName.draw(batch, yourScoreName, 25, 500);
+        batch.draw(Bag, bag.x, bag.y);
+        for (Rectangle token : tokens) {
+            batch.draw(Token, token.x, token.y);
+        }
+        batch.end();
+
+        if (Gdx.input.isTouched()) {
+            Vector3 touchPos = new Vector3();
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPos);
+            bag.x = touchPos.x - 64 / 2;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) bag.x -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bag.x += 200 * Gdx.graphics.getDeltaTime();
+
+        if (bag.x < 0) bag.x = 0;
+        if (bag.x > 800 - 64) bag.x = 800 - 64;
+        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnToken();
+
+        for (Iterator<Rectangle> iter = tokens.iterator(); iter.hasNext(); ) {
+            Rectangle token = iter.next();
+            token.y -= 200 * Gdx.graphics.getDeltaTime();
+            if (token.y + 64 < 0) iter.remove();
+            if (token.overlaps(bag)) {
+                score++;
+                yourScoreName = "score: " + score;
+                dropSound.play();
+                iter.remove();
+            }
+        }
+    }
 
     public void btnGameoverListener() {
         tbCalculator.addListener(new ChangeListener() {
@@ -91,6 +186,11 @@ public class ScrMessages implements Screen, InputProcessor {
 
     @Override
     public void dispose() {
+            Token.dispose();
+            Bag.dispose();
+            dropSound.dispose();
+            BackMusic.dispose();
+            batch.dispose();
     }
 
     @Override
@@ -132,4 +232,7 @@ public class ScrMessages implements Screen, InputProcessor {
     public boolean scrolled(int amount) {
         return false;
     }
+
+
+
 }
