094626dc66d091556b0d3757fefbfc4c9de498c1
ndimax4 <com4uter$ci>
2018-10-22 13:05:09 -0400

    added ignore

--- core/src/gdx/menu/GamMenu.java
+++ core/src/gdx/menu/GamMenu.java
@@ -1,5 +1,8 @@
 package gdx.menu;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -15,8 +18,11 @@ public class GamMenu extends Game {
     ScrMessages scrMessages;
     ScrScroll scrScroll;
     ScrSimple scrSimple;
-    int nScreen; // 0 for menu, 1 for play, and 2 for game over
-    //Table table = new Table();
+    Stage stage;
+    int nScreen;
+    int rowActors;
+    int columnActors;
+    Table table;
 
     public void updateState(int _nScreen) {
         nScreen = _nScreen;
@@ -38,14 +44,26 @@ public class GamMenu extends Game {
         scrMessages = new ScrMessages(this);
         scrScroll = new ScrScroll(this);
         scrSimple = new ScrSimple(this);
+        table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+        int actorWidth = Gdx.graphics.getWidth() / rowActors;
+        int actorHeight = Gdx.graphics.getHeight() /  columnActors;
+        Actor[] actors = new Actor[rowActors * columnActors];
         updateState(0);
 
+        for (int i = 0; i < rowActors; i++){
+            for (int j = 0; j < columnActors; j++){
+                Actor actor = actors[(i * columnActors) + j];
+                table.add(actor).width(actorWidth).height(actorHeight);
+            }
+            table.row();
+        }
     }
 
     @Override
     public void render() {
         super.render();
-
     }
 
 
